61253555c3cdfd418f98511d9e2b2923389bd937
Vibhav Gupta <vib7523@gmail.com>
2024-09-26 17:17:08 +0530

    location_services imporved

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -5,37 +5,37 @@ const apiUrl = process.env.REACT_APP_API_BASE_URL;
 export default function Signup() {
   const [credentials, setCredentials] = useState({ name: "", email: "", password: "", geolocation: "" })
   // console.log("first_one ",credentials.geolocation);
-  // let [address, setAddress] = useState("");
+  let [address, setAddress] = useState("");
   let navigate = useNavigate()
 
-  // const handleClick = async (e) => {
-  //   e.preventDefault();
-  //   let navLocation = () => {
-  //     return new Promise((res, rej) => {
-  //       navigator.geolocation.getCurrentPosition(res, rej);
-  //     });
-  //   }
-  //   let latlong = await navLocation().then(res => {
-  //     let latitude = res.coords.latitude;
-  //     let longitude = res.coords.longitude;
-  //     return [latitude, longitude]
-  //   })
-  //   // console.log(latlong)
-  //   let [lat, long] = latlong
-  //   // console.log(lat, long)
-  //   const response = await fetch("http://localhost:5000/api/auth/getlocation", {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify({ latlong: { lat, long } })
+  const handleClick = async (e) => {
+    e.preventDefault();
+    let navLocation = () => {
+      return new Promise((res, rej) => {
+        navigator.geolocation.getCurrentPosition(res, rej);
+      });
+    }
+    let latlong = await navLocation().then(res => {
+      let latitude = res.coords.latitude;
+      let longitude = res.coords.longitude;
+      return [latitude, longitude]
+    })
+    // console.log(latlong)
+    let [lat, long] = latlong
+    console.log(lat, long)
+    const response = await fetch(`${apiUrl}/api/auth/getlocation`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ latlong: { lat, long } })
 
-  //   });
-  //   const { location } = await response.json()
-  //   // console.log(location);
-  //   // setAddress(location);
-  //   setCredentials({ ...credentials, [e.target.name]: location })
-  // }
+    });
+    const { location } = await response.json()
+    console.log(location);
+    setAddress(location);
+    setCredentials({ ...credentials, [e.target.name]: location })
+  }
 
   const handleSubmit = async (e) => {
     // e.preventDefault();
@@ -88,13 +88,14 @@ export default function Signup() {
             </div>
 
             <div className="m-3">
-              <div  className="form-label">Address</div>
-              <input type="text" className="form-control" name='geolocation' placeholder='Current address' value={credentials.geolocation} onChange={onChange} aria-describedby="emailHelp" />
+              <label htmlFor="address" className="form-label">Address</label>
+              <fieldset>
+                <input type="text" className="form-control" name='address' placeholder='"Click below for fetching address"' value={address} onChange={(e)=>setAddress(e.target.value)} aria-describedby="emailHelp" />
+              </fieldset>
             </div>
-
-            {/* <div className="m-3">
+            <div className="m-3">
               <button type="button" onClick={handleClick} name="geolocation" className=" btn btn-success">Click for current Location </button>
-            </div> */}
+            </div>
             
             <div className="m-3">
               <div  className="form-label">Password</div>
